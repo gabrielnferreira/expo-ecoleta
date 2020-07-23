e97eb4f91f0a03f2f8e274c22ee879c9c2eb1b20
Gabriel Felipe Nunes Ferreira <gabriel97_felipe@hotmail.com>
2020-07-23 09:36:20 -0300

    Criação do projeto e do componente inicial.

--- App.tsx
+++ App.tsx
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {AppLoading} from 'expo';
+import { StatusBar, View } from 'react-native';
+import Routes from './src/routes';
+import {Ubuntu_700Bold, useFonts} from '@expo-google-fonts/ubuntu';
+import {Roboto_400Regular, Roboto_500Medium} from '@expo-google-fonts/roboto';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Ubuntu_700Bold,
+    Roboto_400Regular,
+    Roboto_500Medium
+  })
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar 
+        barStyle="dark-content" 
+        backgroundColor="transparent"
+        translucent
+        />
+      <Routes />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
